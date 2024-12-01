97ee5317738e51b4658acd607e3d43a90d7ae308
Daniel Githiomi <danielgithiomi@gmail.com>
2024-12-01 21:22:10 +0400

    Initial VoucherScan application commit

---
+++ src/main/java/com/dhosio/voucherscan/VoucherscanApplication.java
@@ -0,0 +1,13 @@
+package com.dhosio.voucherscan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoucherscanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoucherscanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dhosio/voucherscan/VoucherscanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhosio.voucherscan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoucherscanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
