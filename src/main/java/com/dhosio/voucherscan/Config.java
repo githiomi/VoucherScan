e9b5dd3aff9870cd2b1b4e3f95510f918ed00ea2
githiomi <daniel.githiomi@gmail.com>
2024-12-20 14:33:05 +0400

    Feat: Added password bcrypt to hash passwords

--- src/main/java/com/dhosio/voucherscan/Config.java
+++ src/main/java/com/dhosio/voucherscan/Config.java
@@ -8,6 +8,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDate;
 import java.util.Arrays;
@@ -22,15 +24,20 @@ public class Config {
         return args -> {
 
             List<User> users = Arrays.asList(
-                    new User("Faith", "Wamaitha", Gender.FEMALE, "wamaitha@gmail.com", "faithwama", LocalDate.of(1998, 7, 17), Branch.FLIC_EN_FLAC.getBranchId()),
-                    new User("Daniel", "Githiomi", Gender.MALE, "danielgithiomi@gmail.com", "dhosiohoes", LocalDate.of(2001, 8, 27), Branch.GRAND_BAIE.getBranchId()),
-                    new User("Nabila", "Modan", Gender.OTHER, "nabilamodan@icloud.com", "amadmodan", LocalDate.of(2002, 4, 28), Branch.QUATRE_BORNES.getBranchId()),
-                    new User("Yusuf", "Voisin", Gender.MALE, "yusufvoisin@gmail.com", "voisin35k", LocalDate.of(1980, 12, 16), Branch.ROSE_BELLE.getBranchId())
+                    new User("Faith", "Wamaitha", Gender.FEMALE, "wamaitha@gmail.com", encoder().encode("faithwama"), LocalDate.of(1998, 7, 17), Branch.FLIC_EN_FLAC.getBranchId()),
+                    new User("Daniel", "Githiomi", Gender.MALE, "danielgithiomi@gmail.com", encoder().encode("dangit123"), LocalDate.of(2001, 8, 27), Branch.GRAND_BAIE.getBranchId()),
+                    new User("Nabila", "Modan", Gender.OTHER, "nabilamodan@icloud.com", encoder().encode("amadmodan"), LocalDate.of(2002, 4, 28), Branch.QUATRE_BORNES.getBranchId()),
+                    new User("Yusuf", "Voisin", Gender.MALE, "yusufvoisin@gmail.com", encoder().encode("voisin35k"), LocalDate.of(1980, 12, 16), Branch.ROSE_BELLE.getBranchId())
             );
 
             service.saveAllUsers(users);
             System.out.println("Users Added");
 
         };
+    }
+
+    @Bean
+    PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
     }
 }
